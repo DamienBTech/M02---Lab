6db8719c623877777c25849233bd77c1f46b16cb
DamienBTech <dholt35@ivytech.edu>
2022-11-09 18:45:02 -0500

    initial

--- convert.js
+++ convert.js
@@ -2,15 +2,40 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
    // TODO: Complete the function
-  
+   let conButton = document.querySelector("#convertButton")
+   conButton.addEventListener("click", clickHandler)
+   
+   celcius = document.querySelector("#cInput")
+   fahrenheit = document.querySelector("#fInput")
+
+   celcius.addEventListener("input", function(){
+   clearBox(fahrenheit)
+
+   })
+   fahrenheit.addEventListener("input", function(){
+   clearBox(celcius)
+
+   })
+
+
 }
 
+function clearBox(textInput){
+   textInput.value = ""
+   }
 function convertCtoF(degreesCelsius) {
    //TODO: Complete the function
-  Fahrenheit = (Celcius * 9/5 + 32)
+   
+   return
 }
 
 function convertFtoC(degreesFahrenheit) {
    // TODO: Complete the function
-  Celcius = ((Fahrenheit - 32) * 5/9)
+
 }
+
+function clickHandler(){
+
+
+}
+
