e5b29031b81ec11c26fbcf84b580afba96dc6eb8
Damien Holt <96097908+DamienBTech@users.noreply.github.com>
2022-11-09 22:04:18 +0000

    Initial commit

---
+++ convert.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+   // TODO: Complete the function
+  
+}
+
+function convertCtoF(degreesCelsius) {
+   //TODO: Complete the function
+  Fahrenheit = (Celcius * 9/5 + 32)
+}
+
+function convertFtoC(degreesFahrenheit) {
+   // TODO: Complete the function
+  Celcius = ((Fahrenheit - 32) * 5/9)
+}
