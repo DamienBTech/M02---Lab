fdfd49db9bc9be6351b3a6f12cc6d7d851302175
DamienBTech <dholt35@ivytech.edu>
2022-11-10 17:43:42 -0500

    V2

--- convert.js
+++ convert.js
@@ -19,23 +19,55 @@ function domLoaded() {
 
 
 }
-
+//removes from the other box
 function clearBox(textInput){
    textInput.value = ""
    }
+//Converts Fahrenheit to Celcius
+function convertFtoC(degreesFahrenheit) {
+   // TODO: Complete the function
+   let degreesFahrenheit = fahrenheit
+   degreesCelsius = ((degreesFahrenheit - 32) * 5/9)
+    return degreesCelsius
+
+    //Converts Celcius to Fahrenheit
 function convertCtoF(degreesCelsius) {
    //TODO: Complete the function
-   
+   let degreesCelsius = celcius
+   degreesFahrenheit = (degreesCelsius * 9/5 + 32) 
    return
 }
 
-function convertFtoC(degreesFahrenheit) {
-   // TODO: Complete the function
 
 }
-
+//checks for input error, activates the conversion, outputs converstion
 function clickHandler(){
 
+var celcius = document.querySelector("#cInput")
+var fahrenheit = document.querySelector("#fInput")
+var errMsg = document.querySelector("#errorMessage")
 
+if(celcius.value.length > 0){
+   var celciusValue = parsefloat(celcius.value)
+   if (!isNaN(celciusValue)){
+      fahrenheit.value = convertCtoF(celciusValue)
+      errMsg.innerHTML = " "
+   }
+   else{
+      errMsg.innerHTML = celcius.value + "is not a number"
+   }
+
+}
+if(fahrenheit.value.length > 0){
+   var fahrenheitValue = parsefloat(fahrenheit.value)
+   if (!isNaN(fahrenheitValue)){
+      celcius.value = convertCtoF(farenheitValue)
+      errMsg.innerHTML = " "
+   }
+   else{
+      errMsg.innerHTML = fahrenheit.value + "is not a number"
+   }
+
+}
 }
 
